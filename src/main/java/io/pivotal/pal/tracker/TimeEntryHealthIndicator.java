97f7b8d440fe211a5482f1a671180f12d164bd92
Adeyenuwo, Paul <Paul.Adeyenuwo@mastercard.com>
2017-12-20 15:49:48 -0500

    Completed Actuator

---
+++ src/main/java/io/pivotal/pal/tracker/TimeEntryHealthIndicator.java
@@ -0,0 +1,33 @@
+package io.pivotal.pal.tracker;
+
+/**
+ * Created by e063106 on 12/20/17.
+ */
+
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.boot.actuate.health.HealthIndicator;
+import org.springframework.stereotype.Component;
+
+@Component
+public class TimeEntryHealthIndicator implements HealthIndicator {
+
+    private static final int MAX_TIME_ENTRIES = 5;
+    private final TimeEntryRepository timeEntryRepo;
+
+    public TimeEntryHealthIndicator(TimeEntryRepository timeEntryRepo) {
+        this.timeEntryRepo = timeEntryRepo;
+    }
+
+    @Override
+    public Health health() {
+        Health.Builder builder = new Health.Builder();
+
+        if(timeEntryRepo.list().size() < MAX_TIME_ENTRIES) {
+            builder.up();
+        } else {
+            builder.down();
+        }
+
+        return builder.build();
+    }
+}
